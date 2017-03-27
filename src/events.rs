560a3a9c18a00f2f068dbd01df14736dd8f4b097
alanpoon <alanpoon@linux-eup8.suse>
2017-03-27 21:41:16 +0800

    1-3

---
+++ src/events.rs
@@ -0,0 +1,82 @@
+macro_rules! struct_events {
+    ( keyboard: { $( $k_alias:ident : $k_sdl:ident ),* }, 
+    // Match against a pattern
+    else: { $( $e_alias:ident : $e_sdl:pat ),* }) => {
+        
+    use sdl2::EventPump;
+
+    pub struct ImmediateEvents{
+        $( pub $k_alias: Option<bool> ,)*
+        $( pub $e_alias : bool ),*
+    }
+    // which will reset on every frame
+    impl ImmediateEvents {
+        pub fn new() -> ImmediateEvents {
+            ImmediateEvents{
+            $( $k_alias: None ,)*
+            $( $e_alias:false),*
+            }
+            
+        }
+    }
+
+
+    pub struct Events {
+        pump: EventPump,
+        pub now: ImmediateEvents,
+        $( pub $k_alias: bool ,)*
+    }
+
+    impl Events {
+        pub fn new(pump: EventPump) -> Events {
+            Events {
+                pump: pump,
+                now: ImmediateEvents::new(),
+                $( $k_alias: false ),*
+            }
+        }
+
+        pub fn pump(&mut self) {
+            self.now = ImmediateEvents::new();
+
+            for event in self.pump.poll_iter() {
+                use sdl2::event::Event::*;
+                use sdl2::keyboard::Keycode::*;
+
+                match event {
+                    KeyDown { keycode, .. } => match keycode {
+                        $(
+                                    Some($k_sdl) => {
+                                        // Prevent multiple presses when keeping a key down
+                                        // Was previously not pressed?
+                                        if !self.$k_alias {
+                                            // Key pressed
+                                            self.now.$k_alias = Some(true);
+                                        }
+
+                                        self.$k_alias = true;
+                                    }
+                                ),*
+                                _ => {}
+                    },
+                    KeyUp { keycode, .. } => match keycode {
+                                $(
+                                    Some($k_sdl) => {
+                                        // Key released
+                                        self.now.$k_alias = Some(false);
+                                        self.$k_alias = false;
+                                    }
+                                ),*
+                                _ => {}
+                            },
+                            $(
+                                $e_sdl => {
+                                    self.now.$e_alias = true;
+                                }
+                            )*,
+                            _ => {}
+                }
+            }
+        }
+    }
+}}
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,19 @@ use sdl2::pixels::Color;
 use std::thread;
 use std::time::Duration;
 
-mod event;
-use event::Events;
+#[macro_use]
+mod events;
+struct_events![
+    keyboard: {
+        key_escape: Escape,
+        key_up: Up,
+        key_down: Down
+    },
+    else: {
+        quit: Quit { .. }
+    }
+]
+
 fn main() {
     // Initialize SDL2
     let mut sdl_context = sdl2::init().unwrap();
@@ -27,9 +38,10 @@ fn main() {
     let mut events = Events::new(sdl_context.event_pump().unwrap());
     loop {
         events.pump();
-        if events.quit || events.key_escape {
+        if events.now.quit || events.now.key_escape == Some(true) {
             break;
         }
+
         // Render a fully black window
         renderer.set_draw_color(Color::RGB(0, 0, 0));
         renderer.clear();
