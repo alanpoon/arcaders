9d2ef5b2a2d0bba06ea2b693fa8268c929d3dd8e
alanpoon <alanpoon@linux-eup8.suse>
2017-03-27 18:32:22 +0800

    1-2

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,37 @@ use sdl2::pixels::Color;
 use std::thread;
 use std::time::Duration;
 
+mod event;
+use event::Events;
 fn main() {
     // Initialize SDL2
-    let sdl_context = sdl2::init().unwrap();
+    let mut sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
 
     // Create the window
     let window = video.window("ArcadeRS Shooter", 800, 600)
-        .position_centered().opengl()
-        .build().unwrap();
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
 
     let mut renderer = window.renderer()
         .accelerated()
-        .build().unwrap();
+        .build()
+        .unwrap();
 
-    // Render a fully black window
-    renderer.set_draw_color(Color::RGB(0, 0, 0));
-    renderer.clear();
-    renderer.present();
+    // Prepare the events record
+    let mut events = Events::new(sdl_context.event_pump().unwrap());
+    loop {
+        events.pump();
+        if events.quit || events.key_escape {
+            break;
+        }
+        // Render a fully black window
+        renderer.set_draw_color(Color::RGB(0, 0, 0));
+        renderer.clear();
+        renderer.present();
+    }
 
-    thread::sleep(Duration::from_millis(3000));
+
 }
