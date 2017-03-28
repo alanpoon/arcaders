3cec8a5e8646377a515349724e7e9b39e317badb
alanpoon <alan@elixirtech.com>
2017-03-28 20:11:22 +0800

    add frame timing

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     // Initialize SDL2
     let mut sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
 
     // Create the window
     let window = video.window("ArcadeRS Shooter", 800, 600)
@@ -30,11 +31,31 @@ fn main() {
     };
     //create default view using a box
     let mut current_view:Box<View> = Box::new(views::DefaultView);
+    let interval = 1_000/60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
     // Prepare the events record
 
     loop {
+        let now = timer.ticks();
+        let dt = now - before;
+    let elapsed = dt as f64 / 1_000.0;
+        if dt < interval {
+            timer.delay(interval - dt );
+            continue
+        }
+        before = now;
+        fps+=1;
+        if now - last_second > 1_000{
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps=0;
+        }
+        
         context.events.pump();
-        match current_view.render(&mut context,0.01){
+        match current_view.render(&mut context,elapsed){
             ViewAction::None=>context.renderer.present(),
             ViewAction::Quit=>break,
         }
