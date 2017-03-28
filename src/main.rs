f51fe5ea3d959c841be9e8054dceb72c0ac4fec0
alanpoon <alan@elixirtech.com>
2017-03-28 20:38:33 +0800

    relieve default view

--- src/main.rs
+++ src/main.rs
@@ -6,60 +6,11 @@ use sdl2::pixels::Color;
 #[macro_use]
 mod phi;
 mod views;
-use phi::{Events, Phi,View,ViewAction};
+
 
 
 fn main() {
     // Initialize SDL2
-    let mut sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    let mut timer = sdl_context.timer().unwrap();
-
-    // Create the window
-    let window = video.window("ArcadeRS Shooter", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-    // Create Phi Context
-    let mut context = Phi {
-        events: Events::new(sdl_context.event_pump().unwrap()),
-        renderer: window.renderer()
-            .accelerated()
-            .build()
-            .unwrap(),
-    };
-    //create default view using a box
-    let mut current_view:Box<View> = Box::new(views::DefaultView);
-    let interval = 1_000/60;
-    let mut before = timer.ticks();
-    let mut last_second = timer.ticks();
-    let mut fps = 0u16;
-
-    // Prepare the events record
-
-    loop {
-        let now = timer.ticks();
-        let dt = now - before;
-    let elapsed = dt as f64 / 1_000.0;
-        if dt < interval {
-            timer.delay(interval - dt );
-            continue
-        }
-        before = now;
-        fps+=1;
-        if now - last_second > 1_000{
-            println!("FPS: {}", fps);
-            last_second = now;
-            fps=0;
-        }
-        
-        context.events.pump();
-        match current_view.render(&mut context,elapsed){
-            ViewAction::None=>context.renderer.present(),
-            ViewAction::Quit=>break,
-        }
-    }
-
+    ::phi::spawn("ArcadeRS Shooter", |_| Box::new(views::DefaultView));
 
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -32,3 +32,60 @@ pub trait View {
     /// `elapsed` is expressed in seconds.
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
 }
+
+//2nd argument takes closure, Box<View> to relieve the defaultview
+pub fn spawn<F>(title: &str, init: F)
+    where F: Fn(&mut Phi) -> Box<View>
+{
+    let sdl_context = ::sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window(title, 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+    // Create Phi Context
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.renderer()
+            .accelerated()
+            .build()
+            .unwrap(),
+    };
+    //create default view using a box
+    let mut current_view: Box<View> = init(&mut context);
+
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+    // Prepare the events record
+
+    loop {
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+        before = now;
+        fps += 1;
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+        context.events.pump();
+        match current_view.render(&mut context, elapsed) {
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break,
+        }
+    }
+
+}
