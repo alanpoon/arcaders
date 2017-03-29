2f9c32c6020959b7dcbde9998c76accaa7f73b78
alanpoon <alanpoon@linux-eup8.suse>
2017-03-29 10:23:19 +0800

    fmt

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -48,22 +48,22 @@ impl View for ShipView {
         }
 
         // [TODO] Insert the moving logic here
-         let traveled = PLAYER_SPEED * elapsed;
-         let diagonal = (phi.events.key_up ^ phi.events.key_down) && (phi.events.key_left ^ phi.events.key_right);
-         let moved = if diagonal {1.0 / 2.0f64.sqrt()}
-         else {1.0} * PLAYER_SPEED *elapsed;
-         let dx = match (phi.events.key_left,phi.events.key_right){
-             (true,true)|(false,false) =>0.0,
-             (true,false)=>-moved,
-             (false,true)=>moved,
-         };
-         let dy = match (phi.events.key_up,phi.events.key_down){
-              (true,true)|(false,false) =>0.0,
-             (true,false)=>-moved,
-             (false,true)=>moved,
-         };
-         self.player.rect.x +=dx;
-         self.player.rect.y +=dy;
+        let traveled = PLAYER_SPEED * elapsed;
+        let diagonal = (phi.events.key_up ^ phi.events.key_down) &&
+                       (phi.events.key_left ^ phi.events.key_right);
+        let moved = if diagonal { 1.0 / 2.0f64.sqrt() } else { 1.0 } * PLAYER_SPEED * elapsed;
+        let dx = match (phi.events.key_left, phi.events.key_right) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+        let dy = match (phi.events.key_up, phi.events.key_down) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+        self.player.rect.x += dx;
+        self.player.rect.y += dy;
         // Clear the screen
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
