ecede4922e1eed1418d8b25f12c5c07dd25ae0d7
alanpoon <alan@elixirtech.com>
2017-03-30 23:19:56 +0800

    1-7 phi::new

--- src/phi/data.rs
+++ src/phi/data.rs
@@ -17,8 +17,7 @@ impl Rectangle {
         assert!(self.w >= 0.0 && self.h >= 0.0);
 
         // SdlRect::new : `(i32, i32, u32, u32) -> Result<Option<SdlRect>>`
-        Some(SdlRect::new(self.x as i32, self.y as i32, self.w as u32, self.h as u32)
-            )
+        Some(SdlRect::new(self.x as i32, self.y as i32, self.w as u32, self.h as u32))
     }
 
     /// Returns a (perhaps moved) rectangle which is contained by a `parent`
@@ -31,14 +30,22 @@ impl Rectangle {
         }
 
         Some(Rectangle {
-            w: self.w,
-            h: self.h,
-            x: if self.x < parent.x { parent.x }
-               else if self.x + self.w >= parent.x + parent.w { parent.x + parent.w - self.w }
-               else { self.x },
-            y: if self.y < parent.y { parent.y }
-               else if self.y + self.h >= parent.y + parent.h { parent.y + parent.h - self.h }
-               else { self.y },
-        })
+                 w: self.w,
+                 h: self.h,
+                 x: if self.x < parent.x {
+                     parent.x
+                 } else if self.x + self.w >= parent.x + parent.w {
+            parent.x + parent.w - self.w
+        } else {
+            self.x
+        },
+                 y: if self.y < parent.y {
+                     parent.y
+                 } else if self.y + self.h >= parent.y + parent.h {
+            parent.y + parent.h - self.h
+        } else {
+            self.y
+        },
+             })
     }
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -22,6 +22,12 @@ pub struct Phi<'window> {
     pub renderer: Renderer<'window>,
 }
 impl<'window> Phi<'window> {
+    fn new(events: Events, renderer: Renderer<'window>) -> Phi<'window> {
+        Phi {
+            events: events,
+            renderer: renderer,
+        }
+    }
     pub fn output_size(&self) -> (f64, f64) {
         let (w, h) = self.renderer.output_size().unwrap();
         (w as f64, h as f64)
@@ -50,6 +56,7 @@ pub fn spawn<F>(title: &str, init: F)
     let sdl_context = ::sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
     let mut timer = sdl_context.timer().unwrap();
+    let _image_context = ::sdl2::image::init(::sdl2::image::INIT_PNG).unwrap();
 
     // Create the window
     let window = video.window(title, 800, 600)
@@ -59,13 +66,12 @@ pub fn spawn<F>(title: &str, init: F)
         .build()
         .unwrap();
     // Create Phi Context
-    let mut context = Phi {
-        events: Events::new(sdl_context.event_pump().unwrap()),
-        renderer: window.renderer()
-            .accelerated()
-            .build()
-            .unwrap(),
-    };
+    let mut context = Phi::new(Events::new(sdl_context.event_pump().unwrap()),
+                               window.renderer()
+                                   .accelerated()
+                                   .build()
+                                   .unwrap());
+
     //create default view using a box
     let mut current_view: Box<View> = init(&mut context);
 
