10b8e04e219d375a9bc7f4149f7bb90917edd36e
alanpoon <alan@elixirtech.com>
2017-04-01 11:38:18 +0800

    added background

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -4,12 +4,49 @@ use phi::data::Rectangle;
 use std::path::Path;
 use sdl2::render::{Texture, TextureQuery};
 use sdl2::image::LoadTexture;
-
+use sdl2::render::Renderer;
 use sdl2::pixels::Color;
 // Constants
 const PLAYER_SPEED: f64 = 180.0;
 const SHIP_W: f64 = 43.0;
 const SHIP_H: f64 = 39.0;
+const DEBUG: bool = false;
+
+#[derive(Clone)]
+struct Background {
+    pos: f64,
+    // The amount of pixels moved to the left every second
+    vel: f64,
+    sprite: Sprite,
+}
+impl Background {
+    fn render(&mut self, renderer: &mut Renderer, elapsed: f64) {
+        // We define a logical position as depending solely on the time and the
+        // dimensions of the image, not on the screen's size.
+        let size = self.sprite.size();
+        self.pos += self.vel * elapsed;
+        if self.pos > size.0 {
+            self.pos -= size.0
+        }
+        // We determine the scale ratio of the window to the sprite.
+        let (win_w, win_h) = renderer.output_size().unwrap();
+        let scale = win_h as f64 / size.1;
+        // We render as many copies of the background as necessary to fill
+        // the screen.
+        let mut physical_left = -self.pos * scale;
+        while physical_left < win_w as f64 {
+            renderer.copy_sprite(&self.sprite,
+                                 Rectangle {
+                                     x: physical_left,
+                                     y: 0.0,
+                                     w: size.0 * scale,
+                                     h: win_h as f64,
+                                 });
+            physical_left += size.0 * scale;
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 enum ShipFrame {
     UpNorm = 0,
@@ -29,6 +66,9 @@ struct Ship {
 }
 pub struct ShipView {
     player: Ship,
+    bg_back: Background,
+    bg_middle: Background,
+    bg_front: Background,
 }
 impl ShipView {
     pub fn new(phi: &mut Phi) -> ShipView {
@@ -55,6 +95,21 @@ impl ShipView {
                 },
                 sprites: sprites,
                 current: ShipFrame::MidNorm,
+            },
+            bg_back: Background {
+                pos: 0.0,
+                vel: 20.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starBG.png").unwrap(),
+            },
+            bg_middle: Background {
+                pos: 0.0,
+                vel: 40.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starMG.png").unwrap(),
+            },
+            bg_front: Background {
+                pos: 0.0,
+                vel: 80.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starFG.png").unwrap(),
             },
         }
     }
@@ -85,9 +140,18 @@ impl View for ShipView {
         // Clear the screen
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
+        // Render the Backgrounds
+        self.bg_back.render(&mut phi.renderer, elapsed);
+        self.bg_middle.render(&mut phi.renderer, elapsed);
+        // Render the bounding box (for debugging purposes)
+        if DEBUG {
+            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+            phi.renderer.fill_rect(self.player
+                                       .rect
+                                       .to_sdl()
+                                       .unwrap());
+        }
 
-        // Render the scene
-        phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
         let movable_region = Rectangle {
             x: 0.0,
             y: 0.0,
@@ -120,8 +184,11 @@ impl View for ShipView {
             unreachable!()
         };
 
-        phi.renderer.copy_sprite(& self.player.sprites[self.player.current as usize],
+        // Render the ship
+        phi.renderer.copy_sprite(&self.player.sprites[self.player.current as usize],
                                  self.player.rect);
+        // Render the foreground
+        self.bg_front.render(&mut phi.renderer, elapsed);
         ViewAction::None
     }
 }
