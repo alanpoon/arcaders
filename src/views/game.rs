952e3c25dd792984ffb5cfc022b1b20eb85262d5
alanpoon <alan@elixirtech.com>
2017-04-06 23:37:21 +0800

    1-12 code clean entity components

--- src/views/game.rs
+++ src/views/game.rs
@@ -10,8 +10,9 @@ use views::shared::{Background, BgSet};
 
 // Constants
 const PLAYER_SPEED: f64 = 180.0;
-const SHIP_W: f64 = 43.0;
-const SHIP_H: f64 = 39.0;
+const PLAYER_PATH: &'static str = "assets/spaceship.png";
+const PLAYER_W: f64 = 43.0;
+const PLAYER_H: f64 = 39.0;
 const DEBUG: bool = false;
 const ASTEROID_PATH: &'static str = "assets/asteroid.png";
 const ASTEROIDS_WIDE: usize = 21;
@@ -54,50 +55,7 @@ impl Asteroid {
                                              1.0),
         }
     }
-    fn new(phi: &mut Phi) -> Asteroid {
-        let mut asteroid = Asteroid {
-            sprite: Asteroid::get_sprite(phi, 1.0),
-            rect: Rectangle {
-                w: ASTEROID_SIDE,
-                h: ASTEROID_SIDE,
-                x: 128.0,
-                y: 128.0,
-            },
-            vel: 0.0,
-        };
-        asteroid.reset(phi);
-        asteroid
-    }
-    fn reset(&mut self, phi: &mut Phi) {
-        let (w, h) = phi.output_size();
-        self.sprite.set_fps(::rand::random::<f64>().abs() * 20.0 + 10.0);
-        self.rect = Rectangle {
-            w: ASTEROID_SIDE,
-            h: ASTEROID_SIDE,
-            x: w,
-            y: ::rand::random::<f64>().abs() * (h - ASTEROID_SIDE),
-        };
-        self.vel = ::rand::random::<f64>().abs() * 100.0 + 50.0;
-    }
-    fn get_sprite(phi: &mut Phi, fps: f64) -> AnimatedSprite {
-        let asteroid_spritesheet = Sprite::load(&mut phi.renderer, ASTEROID_PATH).unwrap();
-        let mut asteroid_sprites = Vec::with_capacity(ASTEROIDS_TOTAL);
-        for yth in 0..ASTEROIDS_HIGH {
-            for xth in 0..ASTEROIDS_WIDE {
-                if ASTEROIDS_WIDE * yth + xth >= ASTEROIDS_TOTAL {
-                    break;
-                }
-                asteroid_sprites.push(asteroid_spritesheet.region(Rectangle {
-                                                                      x: ASTEROID_SIDE * xth as f64,
-                                                                      y: ASTEROID_SIDE * yth as f64,
-                                                                      w: ASTEROID_SIDE,
-                                                                      h: ASTEROID_SIDE,
-                                                                  })
-                                          .unwrap())
-            }
-        }
-        AnimatedSprite::new(asteroid_sprites, fps)
-    }
+
     fn update(mut self, dt: f64) -> Option<Asteroid> {
         self.rect.x -= dt * self.vel;
         self.sprite.add_time(dt);
@@ -118,6 +76,7 @@ impl Asteroid {
         phi.renderer.copy_sprite(&self.sprite, self.rect);
     }
     fn rect(&self) -> Rectangle {
+
         self.rect
     }
 }
@@ -288,7 +247,7 @@ enum CannonType {
     SineBullet { amplitude: f64, angular_vel: f64 },
 }
 #[derive(Clone, Copy)]
-enum ShipFrame {
+enum PlayerFrame {
     UpNorm = 0,
     UpFast = 1,
     UpSlow = 2,
@@ -299,17 +258,56 @@ enum ShipFrame {
     DownFast = 7,
     DownSlow = 8,
 }
-struct Ship {
+struct Player {
     rect: Rectangle,
     sprites: Vec<Sprite>,
-    current: ShipFrame,
+    current: PlayerFrame,
     cannon: CannonType,
 }
-impl Ship {
+impl Player {
+    pub fn new(phi: &mut Phi) -> Player {
+        // Get the spaceship's sprites
+        let spritesheet = Sprite::load(&mut phi.renderer, PLAYER_PATH).unwrap();
+        let mut sprites = Vec::with_capacity(9);
+
+        for y in 0..3 {
+            for x in 0..3 {
+                sprites.push(spritesheet.region(Rectangle {
+                                                    w: PLAYER_W,
+                                                    h: PLAYER_H,
+                                                    x: PLAYER_W * x as f64,
+                                                    y: PLAYER_H * y as f64,
+                                                })
+                                 .unwrap());
+            }
+        }
+
+        Player {
+            // Spawn the player at the center of the screen, vertically.
+            rect: Rectangle {
+                x: 64.0,
+                y: (phi.output_size().1 - PLAYER_H) / 2.0,
+                w: PLAYER_W,
+                h: PLAYER_H,
+            },
+            sprites: sprites,
+            current: PlayerFrame::MidNorm,
+            cannon: CannonType::RectBullet,
+        }
+    }
+    pub fn render(&self, phi: &mut Phi) {
+        // Render the bounding box (for debugging purposes)
+        if DEBUG {
+            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+            phi.renderer.fill_rect(self.rect.to_sdl().unwrap());
+        }
+        // Render the Player
+        phi.renderer.copy_sprite(&self.sprites[self.current as usize], self.rect);
+    }
     fn spawn_bullets(&self) -> Vec<Box<Bullet>> {
         let cannons_x = self.rect.x + 30.0;
         let cannon1_y = self.rect.y + 6.0;
-        let cannon2_y = self.rect.y + SHIP_H - 10.0;
+        let cannon2_y = self.rect.y + PLAYER_H - 10.0;
         match self.cannon {
             CannonType::RectBullet=>
                 // One bullet at the tip of every cannon
@@ -350,9 +348,69 @@ impl Ship {
         }
 
     }
+    pub fn update(&mut self, phi: &mut Phi, elapsed: f64) {
+        if phi.events.now.key_1 == Some(true) {
+            self.cannon = CannonType::RectBullet;
+        }
+
+        if phi.events.now.key_2 == Some(true) {
+            self.cannon = CannonType::SineBullet {
+                amplitude: 10.0,
+                angular_vel: 15.0,
+            };
+        }
+
+        if phi.events.now.key_3 == Some(true) {
+            // TODO
+        }
+        let traveled = PLAYER_SPEED * elapsed;
+        let diagonal = (phi.events.key_up ^ phi.events.key_down) &&
+                       (phi.events.key_left ^ phi.events.key_right);
+        let moved = if diagonal { 1.0 / 2.0f64.sqrt() } else { 1.0 } * PLAYER_SPEED * elapsed;
+        let dx = match (phi.events.key_left, phi.events.key_right) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+        let dy = match (phi.events.key_up, phi.events.key_down) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+        self.rect.x += dx;
+        self.rect.y += dy;
+        let movable_region = Rectangle {
+            x: 0.0,
+            y: 0.0,
+            w: phi.output_size().0 * 0.70,
+            h: phi.output_size().1,
+        };
+        self.rect = self.rect.move_inside(movable_region).unwrap();
+        self.current = if dx == 0.0 && dy < 0.0 {
+            PlayerFrame::UpNorm
+        } else if dx > 0.0 && dy < 0.0 {
+            PlayerFrame::UpFast
+        } else if dx < 0.0 && dy < 0.0 {
+            PlayerFrame::UpSlow
+        } else if dx == 0.0 && dy == 0.0 {
+            PlayerFrame::MidNorm
+        } else if dx > 0.0 && dy == 0.0 {
+            PlayerFrame::MidFast
+        } else if dx < 0.0 && dy == 0.0 {
+            PlayerFrame::MidSlow
+        } else if dx == 0.0 && dy > 0.0 {
+            PlayerFrame::DownNorm
+        } else if dx > 0.0 && dy > 0.0 {
+            PlayerFrame::DownFast
+        } else if dx < 0.0 && dy > 0.0 {
+            PlayerFrame::DownSlow
+        } else {
+            unreachable!()
+        };
+    }
 }
 pub struct GameView {
-    player: Ship,
+    player: Player,
     bullets: Vec<Box<Bullet>>,
     asteroids: Vec<Asteroid>,
     bg: BgSet,
@@ -361,40 +419,11 @@ pub struct GameView {
     explosion_factory: ExplosionFactory,
 }
 impl GameView {
-    #[allow(dead_code)]
-    pub fn new(phi: &mut Phi) -> GameView {
-        let bg = BgSet::new(&mut phi.renderer);
-        GameView::with_backgrounds(phi, bg)
-    }
-
     pub fn with_backgrounds(phi: &mut Phi, bg: BgSet) -> GameView {
-        let spritesheet = Sprite::load(&mut phi.renderer, "assets/spaceship.png").unwrap();
-        let mut sprites = Vec::with_capacity(9);
 
-        for y in 0..3 {
-            for x in 0..3 {
-                sprites.push(spritesheet.region(Rectangle {
-                                                    w: SHIP_W,
-                                                    h: SHIP_H,
-                                                    x: SHIP_W * x as f64,
-                                                    y: SHIP_H * y as f64,
-                                                })
-                                 .unwrap());
-            }
-        }
 
         GameView {
-            player: Ship {
-                rect: Rectangle {
-                    x: 64.0,
-                    y: 64.0,
-                    w: SHIP_W,
-                    h: SHIP_H,
-                },
-                sprites: sprites,
-                current: ShipFrame::MidNorm,
-                cannon: CannonType::RectBullet,
-            },
+            player: Player::new(phi),
             bullets: vec![],
             asteroids: vec![],
             bg: bg,
@@ -411,69 +440,17 @@ impl View for GameView {
             return ViewAction::ChangeView(Box::new(::views::main_menu::MainMenuView::with_backgrounds(phi,bg)));
         }
 
-        // [TODO] Insert the moving logic here
-        let traveled = PLAYER_SPEED * elapsed;
-        let diagonal = (phi.events.key_up ^ phi.events.key_down) &&
-                       (phi.events.key_left ^ phi.events.key_right);
-        let moved = if diagonal { 1.0 / 2.0f64.sqrt() } else { 1.0 } * PLAYER_SPEED * elapsed;
-        let dx = match (phi.events.key_left, phi.events.key_right) {
-            (true, true) | (false, false) => 0.0,
-            (true, false) => -moved,
-            (false, true) => moved,
-        };
-        let dy = match (phi.events.key_up, phi.events.key_down) {
-            (true, true) | (false, false) => 0.0,
-            (true, false) => -moved,
-            (false, true) => moved,
-        };
-        self.player.rect.x += dx;
-        self.player.rect.y += dy;
+
         // Clear the screen
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
         // Render the Backgrounds
         self.bg.back.render(&mut phi.renderer, elapsed);
         self.bg.middle.render(&mut phi.renderer, elapsed);
-        // Render the bounding box (for debugging purposes)
-        if DEBUG {
-            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
-            phi.renderer.fill_rect(self.player
-                                       .rect
-                                       .to_sdl()
-                                       .unwrap());
-        }
 
-        let movable_region = Rectangle {
-            x: 0.0,
-            y: 0.0,
-            w: phi.output_size().0 * 0.70,
-            h: phi.output_size().1,
-        };
-        self.player.rect = self.player
-            .rect
-            .move_inside(movable_region)
-            .unwrap();
-        self.player.current = if dx == 0.0 && dy < 0.0 {
-            ShipFrame::UpNorm
-        } else if dx > 0.0 && dy < 0.0 {
-            ShipFrame::UpFast
-        } else if dx < 0.0 && dy < 0.0 {
-            ShipFrame::UpSlow
-        } else if dx == 0.0 && dy == 0.0 {
-            ShipFrame::MidNorm
-        } else if dx > 0.0 && dy == 0.0 {
-            ShipFrame::MidFast
-        } else if dx < 0.0 && dy == 0.0 {
-            ShipFrame::MidSlow
-        } else if dx == 0.0 && dy > 0.0 {
-            ShipFrame::DownNorm
-        } else if dx > 0.0 && dy > 0.0 {
-            ShipFrame::DownFast
-        } else if dx < 0.0 && dy > 0.0 {
-            ShipFrame::DownSlow
-        } else {
-            unreachable!()
-        };
+
+        // Update the player
+        self.player.update(phi, elapsed);
         let mut player_alive = true;
         // Update the Bullet pos
         self.bullets = ::std::mem::replace(&mut self.bullets, vec![])
@@ -518,7 +495,7 @@ impl View for GameView {
                     }
                 }
 
-                // The player's ship is destroyed if it is hit by an asteroid.
+                // The player's Player is destroyed if it is hit by an asteroid.
                 // In which case, the asteroid is also destroyed.
                 if asteroid.rect().overlaps(self.player.rect) {
                     asteroid_alive = false;
@@ -539,7 +516,7 @@ impl View for GameView {
 
         self.bullets = transition_bullets.into_iter().filter_map(MaybeAlive::as_option).collect();
         if !player_alive {
-            println!("The player's ship has been destroyed.");
+            println!("The player's Player has been destroyed.");
         }
         if phi.events.now.key_space == Some(true) {
             self.bullets.append(&mut self.player.spawn_bullets());
@@ -549,25 +526,8 @@ impl View for GameView {
         }
         println!("{}", self.asteroids.len());
 
-        if phi.events.now.key_1 == Some(true) {
-            self.player.cannon = CannonType::RectBullet;
-        }
-
-        if phi.events.now.key_2 == Some(true) {
-            self.player.cannon = CannonType::SineBullet {
-                amplitude: 10.0,
-                angular_vel: 15.0,
-            };
-        }
-
-        if phi.events.now.key_3 == Some(true) {
-            // TODO
-        }
-
-        // Render the ship
-        phi.renderer.copy_sprite(&self.player.sprites[self.player.current as usize],
-                                 self.player.rect);
-
+        //Render the player
+        self.player.render(phi);
         //Render the asteroids
         for asteroid in &self.asteroids {
             asteroid.render(phi);
